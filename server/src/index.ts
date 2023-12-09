8b4be1182da6fbc66bbf849a6efdecfe0d309dae
Dmytro Soroka <dimasoroka17@gmail.com>
2023-12-09 18:43:46 +0200

    Shop Start

---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/user";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/user", userRouter);
+
+mongoose.connect(
+  "mongodb+srv://dimasoroka17:shoppassword@shop.qsetdex.mongodb.net/shop"
+);
+
+app.listen(3001, () => console.log("SERVER STARTED"));
---
+++ server/src/routes/user.ts
@@ -0,0 +1,66 @@
+import { Router, Request, Response, NextFunction } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { IUser, UserModel } from "../models/user";
+import { UserErrors } from "../errors";
+
+const router = Router();
+
+router.post("/register", async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  try {
+    const user = await UserModel.findOne({ username });
+
+    if (user) {
+      return res.status(400).json({ type: UserErrors.USERNAME_ALREADY_EXISTS });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new UserModel({ username, password: hashedPassword });
+    await newUser.save();
+
+    res.json({ message: "User Registered Successfully" });
+  } catch (error) {
+    res.status(500).json({ type: error });
+  }
+});
+
+router.post("/login", async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  try {
+    const user: IUser = await UserModel.findOne({ username });
+
+    if (!user) {
+      return res.status(400).json({ type: UserErrors.NO_USER_FOUND });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ type: UserErrors.WRONG_CREDENTIALS });
+    }
+
+    const token = jwt.sign({ id: user._id }, "qwerty");
+    res.json({ token, userID: user._id });
+  } catch (error) {
+    res.status(500).json({ type: error });
+  }
+});
+
+export const verifyToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    jwt.verify(authHeader, "secret", (error) => {
+      if (error) {
+        return res.sendStatus(403);
+      }
+
+      next();
+    });
+  }
+};
+
+export { router as userRouter };
